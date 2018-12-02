06017003b896e67362f0c80b196cee88ab02c025
waterstar <suising@waterstar.sg>
2018-12-02 22:22:12 +0800

    fix eslint error

--- src/PlaceCard.js
+++ src/PlaceCard.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody,
-  CardTitle, CardSubtitle, Button } from 'reactstrap';
+  CardTitle, CardSubtitle } from 'reactstrap';
 
 const PlaceCard = (props) => {
   const child   = { width: `16em`}
